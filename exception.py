5e52529ca6daf3765b16e58ac04ab735c43be17d
ujjwalkumar2607 <ujjwalkumar2607@gmail.com>
2023-05-16 09:09:35 -0400

    value error exception

--- exception.py
+++ exception.py
@@ -1,1 +1,10 @@
-while True:print('hello')
+while True:
+    print('hello')
+    break
+
+while True:
+    try:
+        int(input("enter number: "))
+        break
+    except ValueError:
+        print("wrong input entered")
